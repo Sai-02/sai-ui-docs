1b3360cc85d7dce5644d41befdae1bffd59a1372
Sai Prashant Saxena <saiprashant.saxena@gmail.com>
2022-02-03 23:52:12 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -2,7 +2,14 @@ import { Skeleton } from "sai-ui";
 function App() {
   return (
     <div className="">
-      <Skeleton variant={"text"}  />
+      <Skeleton variant={"text"} />
+      <Skeleton
+        variant={"rectangle"}
+        customStyles={{
+          width: "10rem",
+          height: "10rem",
+        }}
+      />
     </div>
   );
 }
