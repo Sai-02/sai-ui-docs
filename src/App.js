f29a94fabcbb024b56cd3e08eb08aa080f19e213
Sai Prashant Saxena <saiprashant.saxena@gmail.com>
2022-02-06 01:34:04 +0530

    Installation page added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import Navbar from "./components/Navbar/Navbar";
 import Header from "./components/Header/Header";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Installation from "./components/Installation/Installation";
+import SkeletonPage from "./components/SkeletonPage/SkeletonPage";
 function App() {
   return (
-    <div className="">
-      <Header />
-      <Navbar />
-    </div>
+    <Router>
+      <div className="">
+        <Header />
+        <div className="grid grid-cols-12">
+          <div className="col-span-2 border h-screen">
+            <Navbar />
+          </div>
+          <div className="col-span-10 flex p-4">
+            <Routes>
+              <Route path="/installation" element={<Installation />} />
+              <Route path="/skeleton" element={<SkeletonPage />} />
+            </Routes>
+          </div>
+        </div>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Installation/Installation.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+const Installation = () => {
+  return (
+    <div className="px-4 w-full">
+      <h1 className="text-3xl font-bold ">Installation</h1>
+      <p className="mt-4 text-lg font-normal">
+        Sai-UI is a simple UI library. Its advantage is that it is very much
+        customizable.
+      </p>
+
+      <h2 className="mt-4 text-2xl font-semibold">npm</h2>
+      <div className="mt-3 bg-[#001E3C] text-white rounded-lg p-3 w-96 py-4 text-lg">
+        npm install sai-ui
+      </div>
+      <h2 className="mt-4 text-2xl font-semibold">yarn</h2>
+      <div className="mt-3 bg-[#001E3C] text-white rounded-lg p-3 w-96 py-4 text-lg">
+        yarn add sai-ui
+      </div>
+      <h2 className="text-2xl font-bold mt-8">About</h2>
+      <p className="mt-3 text-lg font-normal">
+        It is a simple frontend library for React. It is very customizable. It
+        provides many components such as Skeleton, Modal, Tooltip. If you like
+        this package feel free to star it on github. If you found any bugs then
+        please open issue in github. I will be glad to work on them.
+      </p>
+    </div>
+  );
+};
+
+export default Installation;
