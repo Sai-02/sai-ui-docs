a07168150675f080772472ba0c8324e0180c539f
Sai Prashant Saxena <saiprashant.saxena@gmail.com>
2022-02-06 01:36:25 +0530

    Navbar updated

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,7 +1,16 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 const Navbar = () => {
-  return <div></div>;
+  return (
+    <nav className="py-4 pt-8 flex items-center justify-center flex-col gap-4 ">
+      <div className="text-[#3E5060] font-bold">
+        <Link to="/installation">Installation</Link>
+      </div>
+      <div className="text-[#3E5060] font-bold">
+        <Link to="/skeleton">Skeleton</Link>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
