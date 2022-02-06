36efc74100f5e491eff02d0b6ef022a99cc64d0f
Sai Prashant Saxena <saiprashant.saxena@gmail.com>
2022-02-06 19:34:00 +0530

    navbar updated

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,12 +1,21 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 const Navbar = () => {
+  const location = useLocation();
   return (
     <nav className="py-4 pt-8 flex items-center justify-center flex-col gap-4 ">
-      <div className="text-[#3E5060] font-bold">
+      <div
+        className={`text-[#3E5060] font-bold ${
+          location.pathname == "/installation" ? "text-[#0072E5] " : ""
+        }`}
+      >
         <Link to="/installation">Installation</Link>
       </div>
-      <div className="text-[#3E5060] font-bold">
+      <div
+        className={`text-[#3E5060] font-bold ${
+          location.pathname == "/skeleton" ? "text-[#0072E5]" : ""
+        }`}
+      >
         <Link to="/skeleton">Skeleton</Link>
       </div>
     </nav>
