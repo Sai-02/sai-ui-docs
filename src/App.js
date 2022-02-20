0976c45ba77b7ac187674e92ccf85ad9e0267d27
Sai Prashant Saxena <saiprashant.saxena@gmail.com>
2022-02-20 15:19:58 +0530

    Default route set to installation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import Navbar from "./components/Navbar/Navbar";
 import Header from "./components/Header/Header";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import Installation from "./components/Installation/Installation";
 import SkeletonPage from "./components/SkeletonPage/SkeletonPage";
 import { useState } from "react";
@@ -27,6 +32,7 @@ function App() {
             <Routes>
               <Route path="/installation" element={<Installation />} />
               <Route path="/skeleton" element={<SkeletonPage />} />
+              <Route path="/" exact element={<Navigate to="/installation" />} />
             </Routes>
           </div>
         </div>
